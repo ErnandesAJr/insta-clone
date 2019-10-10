f8ed05d6ddfd23b1c2f14076529e3c210b870efe
Ernandes Azevedo Junior <eandesjunior@hotmail.com>
2019-10-10 18:14:55 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {Image} from 'react-native';
+
+import logo from './assets/instagram.png';
+
+import {createAppContainer} from 'react-navigation';
+
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Feed from './pages/Feed'
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Feed
+  },{
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerTitle: <Image source={logo}/>,
+      headerStyle: {
+        backgroundColor:'#f5f5f5'
+      }
+    }
+  })
+);
+
+
+export default Routes;
